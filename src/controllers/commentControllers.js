3e33509df91122c9bc6c880de3c8cc7f6fc45b0a
Erin Yoo <yooeh@masters.edu>
2024-07-26 09:09:16 -0700

    getting changes

---
+++ src/controllers/commentControllers.js
@@ -0,0 +1,4 @@
+const {} = require("../queries/commentQueries")
+
+
+module.exports = {}
--- src/routes/restaurantRoutes.js
+++ src/routes/restaurantRoutes.js
@@ -1,5 +1,5 @@
 const { route } = require("../shared/shared");
-const { findUserWithToken, getMe } = require("../queries/userQueries");
+const { findUserWithToken } = require("../queries/userQueries");
 const {
   addRestaurant,
   deleteRestaurants,
@@ -15,6 +15,7 @@ const isLoggedIn = async (req, res, next) => {
     next(error);
   }
 };
+
 
 route.post("/:id/addRestaurant", isLoggedIn, addRestaurant);
 route.get("/:id/allRestaurants", isLoggedIn, displayAllRestaurants);
