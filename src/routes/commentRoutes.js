703860a2acd7db7334087a711b8fbb3cedc9e032
Erin Yoo <yooeh@masters.edu>
2024-07-26 07:37:40 -0700

    accepted incoming changes

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -1,1 +1,27 @@
 const { route } = require("../shared/shared");
+const { findUserWithToken } = require("../queries/userQueries");
+const {
+
+} = require("../controllers/commentControllers");
+
+const isLoggedIn = async (req, res, next) => {
+  try {
+    req.user = await findUserWithToken(req.headers.authorization);
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+route.post("/", );
+route.post("/", );
+route.put("/", isLoggedIn, );
+route.delete("/:id/delete_user", isLoggedIn, );
+
+route.get("/all_users", isLoggedIn, );
+route.get("/:id/user", isLoggedIn, );
+route.get("/:id/me", isLoggedIn, );
+
+
+module.exports = route;
+module.exports.isLoggedIn = isLoggedIn;
